b6263ba55696ff26bd41024045dde4f132a5f528
HamishClulee <hamish.clulee@hotmail.com>
2020-01-08 10:30:30 +0700

    Insert deploy details

--- ship.sh
+++ ship.sh
@@ -10,11 +10,10 @@ rm -R $PWD/server/src/front-end
 mkdir $PWD/server/src/front-end
 cp -a $PWD/front/dist/. $PWD/server/src/front-end
 echo "---------------------- Pushing to GIT. ----------------------------------------"
-# git add .
-# MSG=". Deployed: $DATE"
-# git commit -m "$1 $MSG"npm install -g yarn
-# git push
+git add .
+MSG=". Deployed: $DATE"
+git commit -m "$1 $MSG"
+git push
 echo "---------------------- shiping the mevn'z -------------------------------------"
-# below is an example of how i deploy to digital ocean, feel free to do whatever you want
-# ssh <<enter your ssh address>> 'cd /var/www/mevn-folder-name && git reset --hard && git pull && sudo systemctl restart mevn-app-name.service'
+ssh hamishclulee@welcomeqr.codes 'cd /var/www/welcomeqr.codes && sudo git reset --hard && sudo git pull && sudo systemctl restart welcomeqr.service'
 echo "---------------------- Deploy complete. Have a nice day. ----------------------"
