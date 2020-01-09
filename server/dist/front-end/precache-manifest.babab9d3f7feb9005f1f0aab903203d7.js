0f3eccd709eab3ddb1f3041af41a40ff4584e1e3
HamishClulee <hamish.clulee@hotmail.com>
2020-01-09 08:09:30 +0700

    edit gitignore to allow server dist . Deployed: 2020-01-09 08:09:30

---
+++ server/dist/front-end/precache-manifest.babab9d3f7feb9005f1f0aab903203d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e5216126bafceac8b37",
+    "url": "/css/app.be6af63b.css"
+  },
+  {
+    "revision": "8b1c02453c44f8b5d00a1336408f9ffa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e5216126bafceac8b37",
+    "url": "/js/app.268cb83d.js"
+  },
+  {
+    "revision": "28db881640bdde4a0439",
+    "url": "/js/chunk-vendors.6069eac6.js"
+  },
+  {
+    "revision": "faf630d62a0b6f746c9e700c739a9cd6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
