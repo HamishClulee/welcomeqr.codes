cf3774ef6f58a6bde4a71894fcc62f6ccdf8df9f
HamishClulee <hamish.clulee@hotmail.com>
2020-01-18 12:06:01 +0700

    Improved first version of markdown editor

--- front/src/views/create.vue
+++ front/src/views/create.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="create-con">
 
-    <div class="form-container">
+    <!-- <div class="form-container">
 
         <div class="input-container">
             <label>Your Email</label>
@@ -18,17 +18,15 @@
             <input type="text" />
         </div>
 
+    </div> -->
+
+    <div class="editor-con">
+
+        <textarea :value="input" @input="update" class="editor-actual"></textarea>
+
+        <div v-html="markdown" class="preview-con"></div>
+
     </div>
-
-    <div id="editor">
-
-        <textarea :value="input" @input="update"></textarea>
-
-        <div v-html="markdown"></div>
-
-    </div>
-
-
 
   </main>
 </template>
@@ -43,94 +41,67 @@ export default {
 
         return {
             input: `
-# H1
-## H2
-### H3
-#### H4
-##### H5
-###### H6
+# Heading Style 1
 
-Alternatively, for H1 and H2, an underline-ish style:
 
-Alt-H1
-======
 
-Alt-H2
-------
+
+
+
+
+## Second Heading
+
+
+
+
+
+### Third...
+
+
 
 Emphasis, aka italics, with *asterisks* or _underscores_.
 
+
 Strong emphasis, aka bold, with **asterisks** or __underscores__.
+
 
 Combined emphasis with **asterisks and _underscores_**.
 
+
 Strikethrough uses two tildes. ~~Scratch this.~~
+
 
 1. First ordered list item
 2. Another item
-⋅⋅* Unordered sub-list. 
-1. Actual numbers don't matter, just that it's a number
-⋅⋅1. Ordered sub-list
+3. Actual numbers don't matter, just that it's a number
 4. And another item.
 
-⋅⋅⋅You can have properly indented paragraphs within list items. Notice the blank line above, and the leading spaces (at least one, but we'll use three here to also align the raw Markdown).
 
-⋅⋅⋅To have a line break without a paragraph, you will need to use two trailing spaces.⋅⋅
-⋅⋅⋅Note that this line is separate, but within the same paragraph.⋅⋅
-⋅⋅⋅(This is contrary to the typical GFM line break behaviour, where trailing spaces are not required.)
 
 * Unordered list can use asterisks
 - Or minuses
 + Or pluses
 
+
 [I'm an inline-style link](https://www.google.com)
+
 
 [I'm an inline-style link with title](https://www.google.com "Google's Homepage")
 
-[I'm a reference-style link][Arbitrary case-insensitive reference text]
-
-[I'm a relative reference to a repository file](../blob/master/LICENSE)
-
-[You can use numbers for reference-style link definitions][1]
-
-Or leave it empty and use the [link text itself].
-
-URLs and URLs in angle brackets will automatically get turned into links. 
-http://www.example.com or <http://www.example.com> and sometimes 
-example.com (but not on Github, for example).
-
-Some text to show that the reference links can follow later.
-
-[arbitrary case-insensitive reference text]: https://www.mozilla.org
-[1]: http://slashdot.org
-[link text itself]: http://www.reddit.com
 
 Here's our logo (hover to see the title text):
-
 Inline-style: 
 ![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")
 
-Reference-style: 
-![alt text][logo]
-
-[logo]: https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 2"
 
 Colons can be used to align columns.
+
 
 | Tables        | Are           | Cool  |
 | ------------- |:-------------:| -----:|
 | col 3 is      | right-aligned | $1600 |
 | col 2 is      | centered      |   $12 |
 | zebra stripes | are neat      |    $1 |
-
-There must be at least 3 dashes separating each header cell.
-The outer pipes (|) are optional, and you don't need to make the 
-raw Markdown line up prettily. You can also use inline Markdown.
-
-Markdown | Less | Pretty
---- | --- | ---
-*Still* | renders | **nicely**
-1 | 2 | 3
 `,
         }
     
@@ -143,49 +114,48 @@ Markdown | Less | Pretty
         }
     },
     methods: {
-
         update: debounce(function (e) {
 
             this.input = e.target.value
         
         }, 300)
-
     }
 }
 </script>
 
-<style lang="css" scoped>
-.create-con {
-    min-height: 100vh;
-}
-#editor {
-  margin: 0;
-  height: 100%;
-  font-family: 'Helvetica Neue', Arial, sans-serif;
-  color: #333;
-}
-textarea, #editor div {
-  display: inline-block;
-  width: 49%;
-  min-height: 80vh;
-  height: 100%;
-  vertical-align: top;
-  box-sizing: border-box;
-  padding: 0 20px;
-}
-
-textarea {
-  border: none;
-  border-right: 1px solid #ccc;
-  resize: none;
-  outline: none;
-  background-color: #f6f6f6;
-  font-size: 14px;
-  font-family: 'Monaco', courier, monospace;
-  padding: 20px;
-}
-
-code {
-  color: #f66;
-}
+<style lang="sass" scoped>
+.create-con 
+    min-height: 100vh
+.preview-con
+    display: flex
+    align-items: flex-start
+    flex-direction: column
+    text-align: left
+    width: 49%
+    padding: 20px
+    line-height: 1.2
+.editor-con
+    margin: 0
+    height: 100%
+    vertical-align: top
+    box-sizing: border-box
+    padding: 10px
+    color: #333
+    text-align: left
+    display: flex
+    flex-direction: row
+.editor-actual
+    border: none
+    border-right: 1px solid #ccc
+    resize: none
+    outline: none
+    height: 120vh
+    overflow-y: hidden
+    background-color: #f6f6f6
+    font-size: 14px
+    font-family: 'Monaco', courier, monospace
+    padding: 20px
+    display: inline-block
+    width: 49%
+    min-height: 80vh
 </style>
