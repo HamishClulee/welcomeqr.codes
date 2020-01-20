8ce766b238fd5e4585b92b07a905c8445973e35b
HamishClulee <hamish.clulee@hotmail.com>
2020-01-20 08:42:54 +0700

    deploy squire to server . Deployed: 2020-01-20 08:42:54

---
+++ server/dist/front-end/precache-manifest.1bf8e1e8908a7b91c6ef559b8a7634ea.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3273a260d310547823de",
+    "url": "/css/app.b9d144c9.css"
+  },
+  {
+    "revision": "de34241d5f65ea1bc718",
+    "url": "/css/chunk-10e87356.f7e657ab.css"
+  },
+  {
+    "revision": "924b541f3a6e1a0375b94b0e803cac4f",
+    "url": "/img/qrcode.924b541f.svg"
+  },
+  {
+    "revision": "ff52bf7d9e38441394c2b296c049c4eb",
+    "url": "/img/text.ff52bf7d.svg"
+  },
+  {
+    "revision": "6f7811ba299ebb8ab5cf01ef8c054246",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3273a260d310547823de",
+    "url": "/js/app.8ad73b41.js"
+  },
+  {
+    "revision": "de34241d5f65ea1bc718",
+    "url": "/js/chunk-10e87356.69e81b7f.js"
+  },
+  {
+    "revision": "067685cc10bcaa7a8507",
+    "url": "/js/chunk-2d0b6cf7.d1a9305c.js"
+  },
+  {
+    "revision": "99346035f694af0483ee",
+    "url": "/js/chunk-2d0d34a8.ceb09424.js"
+  },
+  {
+    "revision": "d0a21f6aedc868f71583",
+    "url": "/js/chunk-vendors.2b50952b.js"
+  },
+  {
+    "revision": "faf630d62a0b6f746c9e700c739a9cd6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
