6ff0533339586d2ee648bb18c6d0a8380d6aebd5
HamishClulee <hamish.clulee@hotmail.com>
2020-01-28 13:44:58 +0700

    Add winston for logging server side => deployed: 2020-01-28 13:44:58

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,11 +10,34 @@ import mongoose from 'mongoose'
 import passport from 'passport'
 import bluebird from 'bluebird'
 import multer from 'multer'
-let history = require('connect-history-api-fallback')
-let cors = require('cors')
+import winston from 'winston'
 import { MONGODB_URI, SESSION_SECRET } from './util/secrets'
 
+const history = require('connect-history-api-fallback')
+const cors = require('cors')
 const MongoStore = mongo(session)
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    new winston.transports.File({ filename: '911911error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+})
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+// 
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }))
+}
 
 // Controllers (route handlers)
 import * as homeController from './controllers/home'
