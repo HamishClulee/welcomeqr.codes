c155ffa6d645473283d2a68b10a8b23b19456a72
Hamish Clulee <hamish.clulee@adurolife.com>
2020-02-03 15:02:30 +0700

    Stub login, signup content

--- front/src/components/sitemodal/content/login.vue
+++ front/src/components/sitemodal/content/login.vue
@@ -1,6 +1,20 @@
 <template>
     <div class="login-container">
-        login
+        <div class="field-container">
+            <label class="placey-text">NAME</label>
+            <input type="text" class="form-control" placeholder="Name">
+        </div>
+        <div class="field-container">
+            <label class="placey-text">URL</label>
+            <input type="url" class="form-control" placeholder="URL">
+        </div>
+
+        <div class="modal-footer">
+            <slot name="footer">
+                <button type="submit" class="button">Cancel</button>
+                <button type="submit" class="button">Submit</button>
+            </slot>
+        </div>
     </div>
 </template>
 
