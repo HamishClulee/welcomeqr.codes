04a43bee6d037b88e7e345c8a57cd4bb91516346
HamishClulee <hamish.clulee@hotmail.com>
2020-02-23 08:00:23 +0700

    Attempt to make logs append rather than overwrite => deployed: 2020-02-23 08:00:23

--- server/dist/logger.js
+++ server/dist/logger.js
@@ -5,8 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 const logPath = process.env.NODE_ENV === 'production' ? '/var/www/welcomeqr.codes/logs/all.log' : './logs.log';
-const output = fs_1.default.createWriteStream(logPath);
-const errorOutput = fs_1.default.createWriteStream(logPath);
+const output = fs_1.default.createWriteStream(logPath, { flags: 'w', encoding: null, mode: 0o666 });
+const errorOutput = fs_1.default.createWriteStream(logPath, { flags: 'w', encoding: null, mode: 0o666 });
 const QLog = new console.Console({ stdout: output, stderr: errorOutput });
 exports.default = QLog;
 //# sourceMappingURL=logger.js.map
--- server/src/logger.ts
+++ server/src/logger.ts
@@ -1,8 +1,8 @@
 import fs from 'fs'
 
 const logPath = process.env.NODE_ENV === 'production' ? '/var/www/welcomeqr.codes/logs/all.log' : './logs.log'
-const output = fs.createWriteStream(logPath);
-const errorOutput = fs.createWriteStream(logPath);
-const QLog = new console.Console({ stdout: output, stderr: errorOutput });
+const output = fs.createWriteStream(logPath, { flags: 'w', encoding: null, mode: 0o666 })
+const errorOutput = fs.createWriteStream(logPath, { flags: 'w', encoding: null, mode: 0o666 })
+const QLog = new console.Console({ stdout: output, stderr: errorOutput })
 
 export default QLog
