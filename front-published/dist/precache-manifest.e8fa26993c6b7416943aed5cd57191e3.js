01a43e71843b3b28929a520592a390b83b9587b5
HamishClulee <hamish.clulee@hotmail.com>
2020-08-25 09:17:49 +0700

    Attempt to move published app to prod => deployed: 2020-08-25 09:17:49

---
+++ front-published/dist/precache-manifest.e8fa26993c6b7416943aed5cd57191e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d8b0930332f1b331d55",
+    "url": "/css/app.ea2a7a4b.css"
+  },
+  {
+    "revision": "04d06535d702835e73b93124e25f86d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d8b0930332f1b331d55",
+    "url": "/js/app.e4d9abbe.js"
+  },
+  {
+    "revision": "1d6574cbf2b4967c6dd7",
+    "url": "/js/chunk-vendors.fce03543.js"
+  },
+  {
+    "revision": "6bf72033de7f9e1ec41e4c10573e5d57",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
