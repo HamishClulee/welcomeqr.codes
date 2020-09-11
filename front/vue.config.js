3900c85df50aab64374beb3e8729ccd443067c1b
HamishClulee <hamish.clulee@hotmail.com>
2020-09-11 21:46:00 +0700

    Shared code and build system completed. => deployed: 2020-09-11 21:46:00

--- front/vue.config.js
+++ front/vue.config.js
@@ -3,20 +3,6 @@ const path = require('path')
 const isMainApp = process.env.APP_TYPE === 'main-app'
 const appDir = isMainApp ? 'main-app' : 'subdoms-app'
 
-const buildPageOpts = () => {
-
-    return {
-        // entry for the page
-        entry: path.resolve(__dirname, `${appDir}/src/main.ts`), //'src/index/main.js',
-        // the source template
-        template: path.resolve(__dirname, `${appDir}/public/index.html`), //'public/index.html',
-        // output as dist/index.html
-        filename: 'index.html',
-        // extracted common chunks and vendor chunks.
-        chunks: ['chunk-vendors', 'chunk-common', 'index']
-      }
-}
-
 const addStyleResource = (rule) => {
 
     rule.use('style-resource')
@@ -31,14 +17,18 @@ const addStyleResource = (rule) => {
 
 module.exports = {
     outputDir: path.resolve(__dirname, `${appDir}/dist`),
-    pages: {
-        index: buildPageOpts()
-    },
     chainWebpack: config => {
+
         const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
         types.forEach(type => addStyleResource(config.module.rule('sass').oneOf(type)))
         config.resolve.alias.set('@I', path.resolve(__dirname, '../interfaces'))
         config.resolve.alias.set('@shared', path.resolve(__dirname, './shared'))
+
+        config.plugin("html").tap(args => {
+            args[0].template = path.resolve(__dirname, `${appDir}/index.html`)
+            return args
+        })
+
     },
     devServer: {
         port: isMainApp ? 8080 : 7070
